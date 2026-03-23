570ae1da6041321d782368fe21513383a9519314
Luke McMillan <luke.mcmillan@whales.org>
2026-03-23 15:56:40 +0000

    fix nextauth route handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
 import NextAuth from "next-auth"
 import { authOptions } from "../../../../auth"
-export const GET = NextAuth(authOptions)
-export const POST = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
